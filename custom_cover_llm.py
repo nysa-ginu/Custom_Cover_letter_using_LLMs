5e7cf8fc777ae7ffd96c97a79b8ab1891891c1af
Nysa Ginu <nysapginu111@gmail.com>
2023-07-15 23:16:44 -0400

    Adding environ variable

--- custom_cover_llm.py
+++ custom_cover_llm.py
@@ -3,6 +3,7 @@ This module uses the LLM model (BARD by Google) to generate the content.
 '''
 
 import os
+import streamlit as st
 from bardapi import Bard
 from parsing_job_desc import get_job_info
 from parsing_resume import extract_text_from_pdf
@@ -21,7 +22,7 @@ def generate_cover_letter(resume_path, job_url, selected_option):
         prompt = "Write a cover letter for the given job desription using the resume for the role of " + str(role_name) + " at company " + str(company_name)
         input_text = prompt + " [RESUME] " + resume + " [JOB DESCRIPTION] " + desc
 
-    token = os.environ.get('BARD_TOKEN')
+    token = st.secrets["BARD_TOKEN"]
     bard = Bard(token=token)
     response = bard.get_answer(input_text)['content']
     
