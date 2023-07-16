c3c89af280c983ea8702e59887cda6ec3d537943
Nysa Ginu <nysapginu111@gmail.com>
2023-07-15 23:30:59 -0400

    resolving vulnerability

--- parsing_resume.py
+++ parsing_resume.py
@@ -2,13 +2,13 @@
 This module would convert the pdf into text so that in can given to the Large Langugae Model.
 '''
 
-import pypdf
+import PyPDF2
 
 
 def extract_text_from_pdf(pdf):
     resume_text = ""
 
-    reader = pypdf.PdfReader(pdf)
+    reader = PyPDF2.PdfReader(pdf)
 
     # Iterate through each page in the PDF
     for page in reader.pages:
